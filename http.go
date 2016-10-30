fa6eff7519d38c5f616df9c2f5ce2bec828f9dc8
cescoferraro <francescoaferraro@gmail.com>
2016-10-30 19:14:04 -0200

    WIDEOPENCORS

--- http.go
+++ http.go
@@ -49,3 +49,24 @@ func GracefulStartServer(port int,router *mux.Router) {
 		}
 	}
 }
+
+
+//EnableCORS is a middleware
+func WideOpenCORS() Adapter {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if origin := r.Header.Get("Origin"); origin != "" {
+				w.Header().Set("Access-Control-Allow-Origin", origin)
+				w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+				w.Header().Set("Access-Control-Allow-Headers",
+					"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+			}
+			// Stop here if its Preflighted OPTIONS request
+			if r.Method == "OPTIONS" {
+				toolsLogger.Print("CORS preflight request!")
+				return
+			}
+			h.ServeHTTP(w, r)
+		})
+	}
+}
